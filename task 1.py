c6851d0119280606e1e4a69fd6958d354ea887d7
zeyadelswah <zeyadelswah62@gmail.com>
2025-02-21 13:58:00 +0200

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,14 @@
+temperature = 12
+
+if temperature >= 30:
+    print("It's a hot day, stay hydrated ! ")
+
+if temperature > 20 and temperature < 29 :
+    print("It's a warm day , enjoy the weather !")
+
+if temperature > 10 and temperature < 19 :
+    print("It's a cool day , wear a jacket ! ")
+
+if temperature < 10 :
+    print("It's a cold day , stay warm !")
+
