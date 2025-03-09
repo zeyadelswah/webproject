81d639e327f00f6aeb496064b934b1a7aebff880
zeyad elswah <zeyadelswah62@gmail.com>
2025-03-09 16:40:05 +0200

    Add files via upload

---
+++ exam1.py
@@ -0,0 +1,22 @@
+library_book =[{"title" : "it" },
+               {"title" : "ds"},
+               {"title": "python"}]
+
+library_book.append({"title" : "ai"})
+
+print(library_book)
+
+library_book.remove({"title" : "ds"})
+
+print(library_book)
+
+
+while input(exit) :
+  print(f"good bye")  
+  break
+
+
+
+
+  
+  
